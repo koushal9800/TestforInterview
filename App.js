29a87a518b5a65e107d4a9b5b4c39d4bf0e87f86
koushal9800 <42782510+koushal9800@users.noreply.github.com>
2020-08-01 10:08:48 +0530

    hello

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import { StyleSheet, Text, View,StatusBar,TextInput,Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default class App extends React.Component {
+  state = {
+    newTodoItem: ''
+  };
+
+  newTodoItemController = textValue => {
+    this.setState({
+      newTodoItem: textValue
+    });
+  };
+
+  
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <LinearGradient style={styles.container} colors={['#DA4453', '#89216B']}>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.appTitle}>Minimalist Todo App</Text>
+
+    <View style={styles.card}>
+    <TextInput
+  style={styles.input}
+  placeholder={'Add an item!'}
+  value={newTodoItem}
+  onChangeText={this.newTodoItemController}
+  placeholderTextColor={'#999'}
+  returnKeyType={'done'}
+  autoCorrect={false}
+/>
+</View>
+    </LinearGradient>
+  )
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
+    // justifyContent: 'center'
   },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 60,
+    marginBottom: 30,
+    fontWeight: '300'
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    width:'90%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50,50,50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 5
+      }
+    })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24
+  }
 });
